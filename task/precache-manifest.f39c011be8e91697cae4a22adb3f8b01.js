c6b10f1f38ba3314dfaf5826ed28c5a48b2822d8
Tadeusz Rychter <tadeusz.rychter@gmail.com>
2020-03-29 20:06:43 +0200

    upload task

---
+++ task/precache-manifest.f39c011be8e91697cae4a22adb3f8b01.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "595cde4f4fc052d08fef6f25592faae6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e44e3b8011fda4541e7b",
+    "url": "/static/css/2.2569bcf6.chunk.css"
+  },
+  {
+    "revision": "48a365a357298df99e3c",
+    "url": "/static/css/main.443504f4.chunk.css"
+  },
+  {
+    "revision": "e44e3b8011fda4541e7b",
+    "url": "/static/js/2.cb2eac93.chunk.js"
+  },
+  {
+    "revision": "2b2cfe83401d9a8a40a351753859216b",
+    "url": "/static/js/2.cb2eac93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48a365a357298df99e3c",
+    "url": "/static/js/main.acbc25b2.chunk.js"
+  },
+  {
+    "revision": "d70d18da37cca1ba9ec2",
+    "url": "/static/js/runtime-main.4f601419.js"
+  },
+  {
+    "revision": "05f1cdadfe476395f60e233b15c22155",
+    "url": "/static/media/icons-16.05f1cdad.eot"
+  },
+  {
+    "revision": "3c1c220e7a18286503fb431c7a7fe183",
+    "url": "/static/media/icons-16.3c1c220e.woff"
+  },
+  {
+    "revision": "3cde8748332d1de6b1ae1c2dc5850754",
+    "url": "/static/media/icons-16.3cde8748.ttf"
+  },
+  {
+    "revision": "0a5c76518a68c185baa2c6744456918c",
+    "url": "/static/media/icons-20.0a5c7651.eot"
+  },
+  {
+    "revision": "51ec31f302d0072808e1f83f85fea4cd",
+    "url": "/static/media/icons-20.51ec31f3.ttf"
+  },
+  {
+    "revision": "cef8cdbb9d0ba82e6e19fb0eeba2ac3d",
+    "url": "/static/media/icons-20.cef8cdbb.woff"
+  }
+]);
